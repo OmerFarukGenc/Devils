6332050438fd0a8fba3f73c6f5ca09af2e0f1c52
omer <genc23tab@gmail.com>
2021-05-09 18:15:39 +0300

    Some bootstrap

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express")
 const app = express()
 const port = 3000
 const mainRoute = require("./routes/index")
+const path = require("path")
 
 app.set("views","./views")
 app.set("view engine","pug")
 
+
+app.use(express.static(path.join(__dirname ,"/public")))
 app.use("/",mainRoute)
 
 app.listen(process.env.PORT || 3000)
