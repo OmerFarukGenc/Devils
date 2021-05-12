6735ee9e2a6fdf6f26c0c8fb062dc8f8b23375f9
omer <genc23tab@gmail.com>
2021-05-12 09:19:49 +0300

    Twitter widget add

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const router = express.Router()
-const aboutRoute = require("./about/index")
-const wisdomRoute = require("./wisdom/index")
-const newsRoute = require("./news/index")
+const express = require("express");
+const router = express.Router();
+const aboutRoute = require("./about/index");
+const wisdomRoute = require("./wisdom/index");
+const newsRoute = require("./news/index");
 
-router.use("/news",newsRoute)
-router.use("/about",aboutRoute)
-router.use("/wisdom",wisdomRoute)
+router.use("/news", newsRoute);
+router.use("/about", aboutRoute);
+router.use("/wisdom", wisdomRoute);
 
-router.get("/",(req,res) => {
-    res.render("index")
-})
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-module.exports = router
+module.exports = router;
